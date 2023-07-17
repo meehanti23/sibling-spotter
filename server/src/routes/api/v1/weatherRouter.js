412848f886b67857d1f1999b7aecaf1a57e6d63b
Tim Meehan <meehanti23@gmail.com>
2023-07-17 17:50:11 -0400

    Some styling

--- server/src/routes/api/v1/weatherRouter.js
+++ server/src/routes/api/v1/weatherRouter.js
@@ -17,4 +17,19 @@ weatherRouter.get("/weather", async (req, res) => {
     }
 });
 
+weatherRouter.get("/jeremyWeather", async (req, res) => {
+    const { q } = req.query
+    try {
+        const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=0df629ef6dde42a7bc2220147231307&q=${q}&aqi=no`)
+        const weatherData = response.data.current
+        console.log(weatherData, "this is the weather data")
+        console.log(weatherData, "this is the weather data")
+        res.status(200).json(weatherData);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ errors: error });
+    }
+});
+
 export default weatherRouter;
